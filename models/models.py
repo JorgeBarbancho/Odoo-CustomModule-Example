125997d72c38fbf05363c090e06b5b4a6ebb5774
mlvillarroya <mlvillarroya@gmail.com>
2020-09-22 22:57:00 +0200

    metodes

--- models/models.py
+++ models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 from odoo.modules.module import get_module_resource
 import base64
 from dateutil.relativedelta import *
-from datetime import date
+from datetime import date, datetime
 
 
 class Book(models.Model):
@@ -16,40 +16,51 @@ class Book(models.Model):
         image_path = get_module_resource('bookdatabase', 'static/src/img', 'default_book.png')
         return base64.b64encode(open(image_path, 'rb').read())
 
-    name = fields.Char(size=32, string='Book\'s name', index=True)
+    name = fields.Char(size=32, string='Book\'s name',required=True, index=True)
     date_publish = fields.Date('Publish date')
     cover = fields.Image(default=_default_image)
     author_id = fields.Many2one('bookdatabase.author','Author')
     publisher_id = fields.Many2one('bookdatabase.publisher','Publisher')
     description = fields.Text()
     active = fields.Boolean('Active', default=True)
-    creator_id = fields.Many2one('res.partner','Created by')
+    creator_id = fields.Many2one('res.users','Created by')
     comment_ids = fields.One2many('bookdatabase.comment','book_id','Comments')
 
 class Author(models.Model):
     _name = 'bookdatabase.author'
     _description = 'Database\'s authors'
+    _order = 'last_name'
+    _inherit = ['image.mixin']
 
     @api.model
     def _default_image(self):
         image_path = get_module_resource('bookdatabase', 'static/src/img', 'default_writer.png')
         return base64.b64encode(open(image_path, 'rb').read())
 
-    name = fields.Char(size=32, string='Author\'s name')
-    last_name = fields.Char(size=32, string='Author\'s last name', index=True)
+    name = fields.Char(size=32, string='Author\'s name',required=True)
+    last_name = fields.Char(size=32, string='Author\'s last name', required=True, index=True)
+    image = fields.Image(default=_default_image)
     birthdate = fields.Date('Birthdate')
+    is_dead = fields.Boolean('Is dead?')
     deceasedate = fields.Date('Decease date')
     age = fields.Integer('Age', compute='_age_compute', readonly=True)
     book_ids = fields.One2many('bookdatabase.book','author_id','Books')
 
-    @api.depends('birthdate')
+    @api.depends('birthdate','is_dead','birthdate')
     def _age_compute(self):
         today = date.today()
         for record in self:
-            if not record.deceasedate:
+            if not record.is_dead:
                 record.age = relativedelta(today, record.birthdate).years
             else:
                 record.age = None
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.name + ' ' + record.last_name
+            result.append((record.id, name))
+        return result
 
 class Publisher(models.Model):
     _name = 'bookdatabase.publisher'
@@ -63,9 +74,18 @@ class Comment(models.Model):
     _name = 'bookdatabase.comment'
     _order = 'datetime_publish'
 
+    @api.model
+    def _myself(self):
+        return self.env.user
+
+    @api.model
+    def _book(self):
+        libro=self.env.context.get('book_id')
+        return libro
+
     name = fields.Char(size=32, string="Subject")
-    datetime_publish = fields.Datetime('Publish date')
-    book_id = fields.Many2one('bookdatabase.book','Book')
-    creator_id = fields.Many2one('res.partner','Created by')
+    datetime_publish = fields.Datetime('Publish date', default=datetime.now())
+    book_id = fields.Many2one('bookdatabase.book','Book', default=_book)
+    creator_id = fields.Many2one('res.users','Created by', default=_myself)
     description = fields.Text()
     active = fields.Boolean('Active', default=True)
